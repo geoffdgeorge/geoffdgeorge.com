b8dad4ef0cacad4b4100b2a1dc4e261204bc0908
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-08-31 18:55:36 -0500

    Added images to "src" folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Geoff George" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
