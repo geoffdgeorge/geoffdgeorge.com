b9674a165b36d5759b22bd2b5ea412995bd7185b
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-09-02 15:25:12 -0500

    Recreated header and intro section as React components

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,63 @@
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+const Header = ({ siteTitle }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(
+        relativePath: { eq: "personal/High-Res-Square.jpg" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <header>
+      <nav>
+        <Img
+          fluid={data.placeholderImage.childImageSharp.fluid}
           style={{
-            color: `white`,
-            textDecoration: `none`,
+            width: `10rem`,
+            height: `10rem`,
+            border: `0.1rem rgb(196, 196, 196) solid`,
+            borderRadius: `50%`,
           }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+          alt="A portrait photo of me"
+        />
+        <h1 className="head-el head-el-2s">{siteTitle}</h1>
+        <ul className="row">
+          <li className="head-el head-el-3s">
+            <Link to="/" id="about-btn">
+              About
+            </Link>
+          </li>
+          <li className="head-el head-el-4s">
+            <Link to="/page-2" id="portfolio-btn">
+              Portfolio
+            </Link>
+          </li>
+          <li className="head-el head-el-5s">
+            <Link to="/" id="cv-btn">
+              CV
+            </Link>
+          </li>
+          <li className="head-el head-el-6s">
+            <Link to="/" id="contact-btn">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,37 @@ import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Geoff George" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="About" />
+    <section id="opening-section">
+      <h1 class="opening-h1">Hey there!</h1>
+      <p>
+        I'm a longtime magazine writer and editor turned web developer and
+        programmer. I first caught the coding bug while reading Paul Ford's
+        issue-length{' '}
+        <a
+          href="https://www.bloomberg.com/graphics/2015-paul-ford-what-is-code/"
+          target="_blank"
+          class="bio-link"
+        >
+          "What Is Code?"
+        </a>
+        {' '}piece in <em>Bloomberg Businessweek</em>, and after a few
+        years&mdash;dabbling in web tutorials here and spending time on{' '}
+        <a href="https://www.freecodecamp.org" target="_blank" class="bio-link">
+          FreeCodeCamp.org
+        </a>
+        {' '}there&mdash;I committed to and completed Northwestern University's
+        coding boot camp.
+      </p>
+      <p>
+        Now I'm leveraging my know-how of syntax and language and my precision
+        as an editor to get up to speed with new languages and libraries quickly
+        and write clean code for web and mobile apps. I'm combining my new
+        skills with my experience as a content creator and deadline-driven
+        project manager to continue to move beyond the printed page into
+        full-stack development.
+      </p>
+    </section>
   </Layout>
 )
 
