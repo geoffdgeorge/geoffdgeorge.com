ad1b1850fd8cad866fa08922167bac05f9aae11b
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-09-01 19:24:29 -0500

    Added normalize.css, style.css, and font files to gatsby-browser.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,13 +5,10 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-import Header from "./header"
-import "./layout.css"
-import "typeface-noto-serif-jp"
-import "typeface-nunito-sans"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
+import Header from './header'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
