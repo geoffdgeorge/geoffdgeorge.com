97a68cb85adcb41ddceba14fb84b51379989d1b1
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-11-11 13:13:01 -0600

    Modified seo.js to generate new description

--- src/components/seo.js
+++ src/components/seo.js
@@ -32,7 +32,7 @@ function SEO({ description, lang, meta, title }) {
       htmlAttributes={{
         lang,
       }}
-      title={title}
+      title={`${site.siteMetadata.title} | ${title}`}
       titleTemplate={`${site.siteMetadata.title} | %s`}
       meta={[
         {
